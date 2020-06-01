9af53044e925cbc6cba17f5ca728d70b2e1ebb66
lisheng1979 <skyeyes@263.net>
2020-05-31 23:44:14 -0700

    Completed module

--- src/app.js
+++ src/app.js
@@ -3,4 +3,6 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
