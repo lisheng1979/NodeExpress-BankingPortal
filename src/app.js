5dfac725c62c60acea22c36a1a372f57a17347da
lisheng1979 <skyeyes@263.net>
2020-06-01 22:19:31 -0700

    Completed module

--- src/app.js
+++ src/app.js
@@ -30,4 +30,7 @@ app.get("/checking", (req, res) => {
 app.get("/credit", (req, res) => {
   res.render("account", { account: accounts.credit });
 });
+app.get("/profile", (req, res) => {
+  res.render("profile", { user: users[0] });
+});
 app.listen(3000, () => console.log("PS Project Running on port 3000."));
