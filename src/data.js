7525d3c477e7263a1e57b41969e609f2ab54713a
lisheng1979 <skyeyes@263.net>
2020-06-02 00:33:28 -0700

    Completed module

--- src/data.js
+++ src/data.js
@@ -7,3 +7,18 @@ const accountData = fs.readFileSync(
 );
 
 const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync(
+  path.join(__dirname, "json", "users.json"),
+  "utf8"
+);
+const users = JSON.parse(userData);
+const writeJSON = () => {
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(
+    path.join(__dirname, "json/accounts.json"),
+    accountsJSON,
+    "utf8"
+  );
+};
+
+module.exports = { accounts, users, writeJSON };
