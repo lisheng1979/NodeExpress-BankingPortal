ada4934978a6e6bcd6a265172ab5a5750629eb01
lisheng1979 <skyeyes@263.net>
2020-06-02 00:14:46 -0700

    Completed module

--- src/app.js
+++ src/app.js
@@ -50,4 +50,21 @@ app.post("/transfer", (req, res) => {
   );
   res.render("transfer", { message: "Transfer Completed" });
 });
+app.get("/payment", (req, res) => {
+  res.render("payment", { account: accounts.credit });
+});
+app.post("/payment", (req, res) => {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount, 10);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(
+    path.join(__dirname, "json/accounts.json"),
+    accountsJSON,
+    "utf8"
+  );
+  res.render("payment", {
+    message: "Payment successful",
+    account: accounts.credit,
+  });
+});
 app.listen(3000, () => console.log("PS Project Running on port 3000."));
