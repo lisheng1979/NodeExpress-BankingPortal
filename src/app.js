c284681e18e395410341bd4c620aab702cca4091
lisheng1979 <skyeyes@263.net>
2020-06-01 22:08:26 -0700

    Completed module

--- src/app.js
+++ src/app.js
@@ -18,7 +18,16 @@ const userData = fs.readFileSync(
   "utf8"
 );
 const users = JSON.parse(userData);
-app.get("/", (req, res) =>
-  res.render("index", { title: "Account Summary", accounts })
-);
+app.get("/", (req, res) => {
+  res.render("index", { title: "Account Summary", accounts });
+});
+app.get("/savings", (req, res) => {
+  res.render("account", { account: accounts.savings });
+});
+app.get("/checking", (req, res) => {
+  res.render("account", { account: accounts.checking });
+});
+app.get("/credit", (req, res) => {
+  res.render("account", { account: accounts.credit });
+});
 app.listen(3000, () => console.log("PS Project Running on port 3000."));
